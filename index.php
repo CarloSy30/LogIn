045a16ff5855837cdeadfc77a7f992ed0d2f7714
Dan Carlo Sy <dancarlosyyyyy30@gmail.com>
2024-03-19 22:28:47 +0800

    modify p

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             ?>
                                 <div class="header-text mb-4" id="right-header">
                                     <h2>Hello,Again, DAN CARLO SY</h2>
-                                    <p>We are happy to have you back.</p>
+                                    <p>We are happy to have you back......</p>
                                 </div>
                                 <div class="input-group mb-3">
                                     <input type="text" name="email" value = "<?php echo $data = isset($_POST['email']) ? htmlentities($_POST['email']) : null ?>" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
