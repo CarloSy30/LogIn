1c96ded550793459b545ae82fc9e0c4bb1a87a2a
Dan Carlo Sy <dancarlosyyyyy30@gmail.com>
2024-03-19 21:23:09 +0800

    another test

--- forgotPassword.php
+++ forgotPassword.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     // $signupview = new signupview();
     // $result = $signupview->showAlert($requested);
 
-    echo $forgotPasswordcontr->verifyAccount();
+    echo $forgotPasswordcontr->clickVerify();
 
 }
 
